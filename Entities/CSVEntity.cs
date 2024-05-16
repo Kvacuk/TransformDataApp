705f051070fc5a0d475eadaba0750820cc2b1803
Bohdan Papirniak <bohdan.papirniak@jadexconsulting.com>
2024-05-16 13:39:44 +0300

    Initial commit

---
+++ Entities/CSVEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformDataApp.Entities
+{
+    public class CSVEntity
+    {
+        public int VendorID { get; set; }
+        public DateTime tpep_pickup_datetime {  get; set; }
+        public DateTime tpep_dropoff_datetime { get; set; }
+        public int passenger_count { get; set; }
+        public double trip_distance { get; set; }
+        public int RatecodeID { get; set; }
+        public char store_and_fwd_flag { get; set; }
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public int payment_type { get; set; }
+        public double fare_amount { get; set; }
+        public double extra { get; set; }
+        public double mta_tax {  get; set; }
+        public double tip_amount { get; set; }
+        public double tolls_amount { get; set; }
+        public double improvement_surcharge { get; set; }
+        public double total_amount { get; set; }
+        public double congestion_surcharge { get; set; }
+    }
+}
---
+++ Entities/StoredEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformDataApp.Entities
+{
+    public class StoredEntity
+    {
+        public DateTime tpep_pickup_datetime { get; set; }
+        public DateTime tpep_dropoff_datetime { get; set; }
+        public int passenger_count { get; set; }
+        public double trip_distance { get; set; }
+        public string store_and_fwd_flag { get; set; }
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public double fare_amount { get; set; }
+        public double tip_amount { get; set; }
+    }
+}
