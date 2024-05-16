93afe39afaa164c640110f21ea8b5c42dbcc1c97
Bohdan Papirniak <bohdan.papirniak@jadexconsulting.com>
2024-05-16 13:54:36 +0300

    fix gitignore

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿
+using CsvHelper;
+using System.Globalization;
+using TransformDataApp.Entities;
+
+using (var reader = new StreamReader("https://drive.google.com/file/d/1l2ARvh1-tJBqzomww45TrGtIh5j8Vud4/view?usp=sharing"))
+    using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture) )
+{
+    var records = csv.GetRecord<CSVEntity>();
+
+}
